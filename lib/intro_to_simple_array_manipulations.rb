45f5b35c0e4e185c06bb4b87c76dac30e1f292aa
Neal JIn Oh <no4259@gmail.com>
2019-08-14 17:31:46 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array,string)
+ array.push(string)
+end
+
+def using_unshift(array,string)
+ array.unshift(string)
+end
+
+def using_pop(array)
+ array.pop()
+end
+
+def pop_with_args(array)
+ array.pop()
+end
+
+def using_shift(array)
+ array.shift()
+end
+
+def shift_with_args(array)
+ array.shift()
+end
+
+def using_concat(array,element)
+ array.concat(element)
+end
+
+def using_insert(array,element)
+ array.insert(element)
+end
+
+def using_uniq(array)
+ array.uniq()
+end
+
+def using_flatten(array)
+ array.flatten()
+end
+
+def using_delete(array,element)
+ array.delete(element)
+end
+
+def using_delete_at(array,Integer)
+ array.delete(Integer)
+end
